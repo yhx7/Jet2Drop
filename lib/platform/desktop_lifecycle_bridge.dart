67dde2778414064dcbfc2d21850ca8b73efde371
YuHaoxuan <2210319939@qq.com>
2026-09-17 23:12:15 +0800

    style: keep dart format clean across lib and test

--- lib/platform/desktop_lifecycle_bridge.dart
+++ lib/platform/desktop_lifecycle_bridge.dart
@@ -3,9 +3,7 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 
 class DesktopLifecycleBridge {
-  static const _windowsChannel = MethodChannel(
-    'jet2drop/windows_lifecycle',
-  );
+  static const _windowsChannel = MethodChannel('jet2drop/windows_lifecycle');
   static const _macosChannel = MethodChannel('jet2drop/macos_lifecycle');
 
   static MethodChannel? get _channel {
